a2f8160721b27d6e3e40443934e4e5d2248a842b
David <robelene112@gmail.com>
2025-08-12 22:55:09 +0200

    Initial commit.

---
+++ helloW.js
@@ -0,0 +1,24 @@
+var ModTemplate = require('../../lib/templates/modtemplate');
+
+class Tutorial01 extends ModTemplate {
+
+  constructor(app) {
+
+    super(app);
+
+    this.name            = "helloW";
+    this.slug            = "hellow";
+    this.description     = "Hello world!";
+    this.categories       = "";
+    return this;
+
+  }
+
+  async render() { 
+    if (document.querySelector('body')) {
+       document.querySelector('body').innerHTML = "Hello World";  
+    }
+  }
+}
+
+module.exports = Tutorial01;
